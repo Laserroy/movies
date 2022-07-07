f33aac8d091ef61fc32b7340df929ae3b2d07c33
Pavel Cherkasov <deectro@gmail.com>
2022-07-07 10:47:51 +0300

    Add router

--- Public/index.php
+++ Public/index.php
@@ -3,10 +3,27 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../bootstrap.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
+$router = new \Klein\Klein();
 
-match([$method, $uri]) {
-    ['GET', '/'] => (new \App\Controllers\MovieController)->index(),
-};
+$router->respond('GET', '/?', function () {
+    echo (new \App\Controllers\MovieController)->index();
+});
+
+$router->respond('GET', '/[i:id]', function ($request) {
+    echo (new \App\Controllers\MovieController)->show($request->id);
+});
+
+$router->respond('GET', '/create', function () {
+    echo (new \App\Controllers\MovieController)->create();
+});
+
+$router->respond('POST', '/?', function ($request) {
+    echo (new \App\Controllers\MovieController)->store($request);
+});
+
+$router->respond('DELETE', '/[i:id]', function ($request) {
+    echo (new \App\Controllers\MovieController)->delete($request->id);
+});
+
+$router->dispatch();
 
